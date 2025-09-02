0f2c6c9a42267f026e52767cc1d2fff7781bc3ae
Artyom Rashevsky <albatroz3000@yandex.ru>
2025-09-02 23:10:42 +0300

    Update run example

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-set -e
-python -m src.main       --csv_path ./data/data.csv       --image_folder ./data/imgs       --text_model_dir ./text_model       --clip_model ./image_model/ViT-B-32.pt       --classifier_path ./image_model/classifier_checkpoint3.pth       --tabular_model_path ./table_model/f1_pipeline_final.pkl       --id_col id --name_col name_rus --desc_col description       --out_csv ./results/submission.csv       --agg weighted --w_text 0.33 --w_img 0.34 --w_tab 0.33
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+mkdir -p ./results
+
+python -m src.main \
+  --csv_path ./data/data.csv \
+  --image_folder ./data/imgs \
+  --text_model_dir ./text_model \
+  --clip_model ./image_model/ViT-B-32.pt \
+  --classifier_path ./image_model/classifier_checkpoint3.pth \
+  --tabular_model_path ./f1_pipeline_final.pkl \
+  --id_col id --name_col name_rus --desc_col description \
+  --agg weighted --w_text 0.33 --w_img 0.34 --w_tab 0.33 \
+  --out_csv ./results/submission.csv \
+  "$@"
